d9a0a763ad0a64fb063f3383dcdc9fe8406599e5
Zhang Wenqing <winking.zhang@grapecity.com>
2017-01-05 21:40:14 +0800

    implement simple http hello with Dockerfile

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"runtime"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello world, I'm running on %s with an %s CPU ",
+		runtime.GOOS, runtime.GOARCH)
+}
+
 func main() {
-	println("hello world")
+	http.HandleFunc("/", indexHandler)
+	hostAndPort := ":8080"
+	// fix for run without firewall warning in window
+	if os.Getenv("GOPRODUCT") != "1" {
+		hostAndPort = "localhost:8080"
+	}
+	http.ListenAndServe(hostAndPort, nil)
 }
