92778979e3336cde7eae32bee3694c0332f057bd
Zhang Wenqing <winking.zhang@grapecity.com>
2017-01-08 20:56:07 +0800

    fix reverse proxy headers

--- main.go
+++ main.go
@@ -11,11 +11,20 @@ func main() {
 	http.ListenAndServe(":8080", &httputil.ReverseProxy{
 		Director: func(r *http.Request) {
 			log.Printf("httplog> %v %v %v (%q)", r.RemoteAddr, r.Method, r.Host, r.RequestURI)
-			r.Header.Set("X-Proxy-Secret", "Secret")
-			r.Header.Set("Host", r.Host)
+
+			// set reverse proxy target
 			r.URL.Scheme = "http"
 			r.URL.Host = "localhost:8081"
-			r.RequestURI = ""
+			r.URL.Path = "/api/hello"
+
+			if _, ok := r.Header["User-Agent"]; !ok {
+				// explicitly disable User-Agent so it's not set to default value
+				r.Header.Set("User-Agent", "")
+			}
+
+			// additional header
+			r.Header.Set("X-Proxy-Secret", "Secret")
+			r.Header.Set("Host", r.Host)
 		},
 	})
 }
