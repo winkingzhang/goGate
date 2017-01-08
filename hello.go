98a6b69876b0b7d332aa4889a409506ea0f0f473
Zhang Wenqing <winking.zhang@grapecity.com>
2017-01-08 20:55:28 +0800

    add a random for better demo showing

--- hello.go
+++ hello.go
@@ -4,18 +4,24 @@ import (
 	"encoding/json"
 	"net/http"
 	"runtime"
+	"time"
+	"math/rand"
 )
 
 type Runtime struct {
 	OS   string    `json:"os"`
-	Arch string   `json:"arch"`
+	Arch string    `json:"arch"`
+	Rand int       `json:"_r"`
 }
+
+var r1 = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 func main() {
 	http.HandleFunc("/api/hello", func(w http.ResponseWriter, r *http.Request) {
 		rt := Runtime{
 			OS: runtime.GOOS,
 			Arch:runtime.GOARCH,
+			Rand: r1.Intn(100),
 		};
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 		w.WriteHeader(http.StatusOK)
@@ -23,6 +29,5 @@ func main() {
 			panic(err)
 		}
 	})
-	hostAndPort := "localhost:8081"
-	http.ListenAndServe(hostAndPort, nil)
+	http.ListenAndServe("localhost:8081", nil)
 }
